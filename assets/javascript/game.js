004869aa4606842519ded5c99f21081bde21153b
Spicy Sampler <50890113+Floses@users.noreply.github.com>
2019-07-23 19:36:25 -0700

    seperated our game into different functions. completed question one with click events displaying right or wrong answer

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,13 @@
 $(document).ready(function() {
+  // declaring a variable to clear our time whenver we need it
   var clearTime = clearTimeout();
+  // this boolean will tell us whether the time is on or not
   var timeOn = false;
+  // this variable is if the timer reachers zero
   var timeRunOut = false;
+  // setting a variable called timer to 20 for 20 seconds of time
+  var timer = 20;
+  // this is our test object. Inside this object we have all of the questions, answer options, and answers for the program
   var test = {
     questions: {
       q1: "Which number should come next in the pattern? 37, 34, 43, 40...",
@@ -33,12 +39,21 @@ $(document).ready(function() {
       q5: "10"
     }
   };
+  // this function will be called upon to decrease our timer and show the time on our html
+  function countDown() {
+    timer--;
 
+    $(".title").html("Time:" + timer);
+  }
+  // this function starts the game by displaying a welcome message and then taking it away
   function startGame() {
     $(".questions").append("<h1 id='welcome'>Welcome to IQ test!</h1>");
     setTimeout(function() {
       $(".questions").replaceWith("<h3>" + test.questions.q1 + "</h3>");
     }, 4000);
+  }
+  // this function will display our first question
+  function question1() {
     setTimeout(function() {
       $(".num1").append(
         "<button type='button' class='btn btn-primary'>" +
@@ -46,6 +61,14 @@ $(document).ready(function() {
           "</button>"
       );
     }, 4000);
+    $(".num1").on("click", function() {
+      $(".gameArea").append(
+        "<h1>Sorry! The answer was:</h1>" + test.options.q1[2]
+      );
+      clearTime;
+      $(".title").replaceWith("");
+      timeOn = false;
+    });
     setTimeout(function() {
       $(".num2").append(
         "<button type='button' class='btn btn-primary'>" +
@@ -53,6 +76,14 @@ $(document).ready(function() {
           "</button>"
       );
     }, 4000);
+    $(".num2").on("click", function() {
+      $(".gameArea").append(
+        "<h1>Sorry! The answer was:</h1>" + test.options.q1[2]
+      );
+      clearTime;
+      $(".title").replaceWith("");
+      timeOn = false;
+    });
     setTimeout(function() {
       $(".num3").append(
         "<button type='button' class='btn btn-primary'>" +
@@ -60,6 +91,12 @@ $(document).ready(function() {
           "</button>"
       );
     }, 4000);
+    $(".num3").on("click", function() {
+      $(".gameArea").append("<h1>Nice Job!</h1>");
+      clearTime;
+      $(".title").replaceWith("");
+      timeOn = false;
+    });
     setTimeout(function() {
       $(".num4").append(
         "<button type='button' class='btn btn-primary'>" +
@@ -67,7 +104,26 @@ $(document).ready(function() {
           "</button>"
       );
     }, 4000);
+    $(".num4").on("click", function() {
+      $(".gameArea").append(
+        "<h1>Sorry! The answer was:</h1>" + test.options.q1[2]
+      );
+      clearTime;
+      $(".title").replaceWith("");
+      timeOn = false;
+    });
+  }
+  // this function starts the timer for the game
+  function timerStart() {
+    if (!timeOn) {
+      setTimeout(function() {
+        interval = setInterval(countDown, 1000);
+        timeOn = true;
+      }, 4000);
+    }
   }
 
   startGame();
+  question1();
+  timerStart();
 });
